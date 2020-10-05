a78276f123ad036c45646b5c50441ae298f5805e
ArturSzczypta <arturszczypta@gmail.com>
2020-10-05 07:52:08 +0100

    Update birthday.py

--- birthday.py
+++ birthday.py
@@ -1,27 +1,32 @@
 import numpy as np
 
 
-iterations = 10
-population = 10
+iterations = 1
+population = 5
 days  = 5
 
 
-raw =  np.zeros((days,population))
+raw =  np.zeros((population,population))
 #print(raw)
 
 for i in range(iterations):
 	print(i)
 	single = np.random.randint(1,high=days+1,size=population)
-	#print(single)
+	print(single)
 
 	for k in range(population):
 		#print('pop',k)
 		unique,counts = np.unique(single[:k+1], return_counts=True)
-
-		for u, c in zip(unique,counts):
+		print(list(zip(unique,counts)))
+		print('---')
+		repeats,quant  = np.unique(counts,return_counts=True)
+		print(list(zip(repeats,quant)))
+		for r, q in zip(repeats,  quant):
 			#print(f'Letter: {u}')
 			#print(f'Number: {c}')
-			raw[u-1][k] += c
+			if q > 1:
+				raw[r-1][k] += q
 			#print(u,k)
 		#print('---')
+		print('----------')
 print(raw)
